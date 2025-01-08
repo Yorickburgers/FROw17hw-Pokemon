ca4188768ee6b71403d13f6efac8848c6ea7743c
yorickburgers <yorickburgers@live.nl>
2025-01-08 16:54:33 +0100

    component geprobeerd te maken

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import './App.css'
+import {useEffect, useState} from "react";
 
 function App() {
+    const [allPokemon, fetchAllPokemon] = useState([]);
+    useEffect(() => {
+        try {
+            async function fetchPokemon() = await
+        } catch(e) {
+            console.error(e);
+        }
+    }, []);
 
-  return (
-    <>
-        <h1>Gotta catch em all!</h1>
-
-    </>
-  )
+    return (
+        <>
+            <h1>Gotta catch em all!</h1>
+            <article>
+                <p>{name}</p>
+                <span>
+                <img src={image} alt={`image of ${name}`} />
+            </span>
+                <p>Moves: {moves}</p>
+                <p>Weight: {weight}</p>
+                <p>Abilities:</p>
+                <ul>
+                    {allPokemon[name]?.abilities.map((ability, index) => {
+                        return (
+                            <li key={index}>{ability}</li>
+                        )
+                    })}
+                </ul>
+            </article>
+        </>
+    )
 }
 
 export default App
