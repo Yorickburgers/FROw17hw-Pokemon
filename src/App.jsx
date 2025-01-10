194818efc5112914044d6bdd4d0c59b195c22b4d
yorickburgers <yorickburgers@live.nl>
2025-01-10 13:48:10 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,95 @@ import PokemonCard from "./components/pokemonCard/PokemonCard.jsx";
 function App() {
     const [allPokemon, setAllPokemon] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [previousPokemon, setPreviousPokemon] = useState("");
+    const [nextPokemon, setNextPokemon] = useState("");
+    const [error, toggleError] = useState(false);
+    const controller = new AbortController();
+
+    async function fetchPokemon() {
+        setLoading(true);
+        toggleError(false);
+        try {
+            const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20");
+            setAllPokemon(response.data.results);
+            setPreviousPokemon(response.data.previous);
+            setNextPokemon(response.data.next);
+            setLoading(false);
+        } catch (e) {
+            console.error(e);
+            setLoading(false);
+            toggleError(true);
+        }
+    }
+
+    async function fetchPreviousPokemon() {
+        setLoading(true);
+        toggleError(false);
+        try {
+            const response = await axios.get(previousPokemon, {
+                signal: controller.signal,
+            });
+            setAllPokemon(response.data.results);
+            setPreviousPokemon(response.data.previous);
+            setNextPokemon(response.data.next);
+            setLoading(false);
+        } catch (e) {
+            console.error(e);
+            setLoading(false);
+            toggleError(true);
+        }
+    }
+
+    async function fetchNextPokemon() {
+        setLoading(true);
+        toggleError(false);
+        try {
+            const response = await axios.get(nextPokemon);
+            setAllPokemon(response.data.results);
+            setPreviousPokemon(response.data.previous);
+            setNextPokemon(response.data.next);
+            setLoading(false);
+        } catch (e) {
+            console.error(e);
+            setLoading(false);
+            toggleError(true);
+        }
+    }
 
     useEffect(() => {
-        async function fetchPokemon() {
-            setLoading(true);
-            try {
-                const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20");
-                console.log(response);
-                setAllPokemon(response.data.results);
-                setLoading(false);
-            } catch (e) {
-                console.error(e);
-                setLoading(false);
-            }
-        }
-            fetchPokemon();
-        }, []);
+        const controller = new AbortController();
 
+        fetchPokemon();
+
+        return function cleanup() {
+            controller.abort();
+        }
+    }, []);
+
+    console.log(allPokemon);
     if (loading) {
         return <div>Loading...</div>;
     }
 
     return (
-        <>
-            <h1>Gotta catch em all!</h1>
-            {allPokemon.map((pokemon) => {
-                return (
-            <PokemonCard
-                key={pokemon.id}
-                name={pokemon.name}
-                image={pokemon.sprites.other["official-artwork"].front_default}
-                moves={pokemon.moves.length}
-                weight={pokemon.weight}
-                abilities={pokemon.abilities}
-            />
-                );
-            })}
-        </>
+        <div className="page-container">
+            <h1 className="title">Gotta catch em all!</h1>
+            <div className="button-wrapper">
+                <button className={`pageButton ${previousPokemon===null && "void"}`} type="button" disabled={previousPokemon === null} onClick={fetchPreviousPokemon}>Vorige</button>
+                <button className={`pageButton ${nextPokemon===null && "void"}`} type="button" disabled={nextPokemon === null} onClick={fetchNextPokemon}>Volgende</button>
+            </div>
+            <div className="pokemonCard-container">
+                {error && <h2 className="errorMessage">Er ging iets mis bij het ophalen van de data... Probeer het nog eens.</h2>}
+                {allPokemon.map((pokemon) => {
+                    return (
+                        <PokemonCard
+                            key={pokemon.name}
+                            url={pokemon.url}
+                        />
+                    );
+                })}
+            </div>
+        </div>
     );
 }
 
--- src/components/pokemonCard/PokemonCard.jsx
+++ src/components/pokemonCard/PokemonCard.jsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import "./PokemonCard.css"
+import axios from "axios";
 
-export function PokemonCard({name, image, moves, weight, abilities}) {
+export function PokemonCard({url}) {
+    const [pokemonDetails, setPokemonDetails] = useState({});
+    const [error2, toggleError2] = useState(false);
+
+useEffect(() => {
+    const controller = new AbortController();
+
+    async function fetchPokemonDetails(){
+        toggleError2(false);
+        try {
+            const response = await axios.get(url, {
+                signal: controller.signal,
+            });
+            setPokemonDetails(response.data);
+            toggleError2(false);
+        } catch (e) {
+            console.error(e);
+            toggleError2(true);
+        }
+    }
+
+    fetchPokemonDetails();
+
+    return function cleanup() {
+        controller.abort();
+    }
+
+}, [url])
+
+    if (Object.keys(pokemonDetails) < 1) {
+        return <div>Loading...</div>;
+    }
+
+    const capitalizedName = pokemonDetails.name[0].toUpperCase() + pokemonDetails.name.slice(1);
+
     return (
         <>
-            <article>
-                <p>{name}</p>
-                <span>
-                <img src={image} alt={`image of ${name}`}/>
-            </span>
-                <p>Moves: {moves}</p>
-                <p>Weight: {weight}</p>
-                <p>Abilities:</p>
-                <ul>
-                    {abilities.map((ability, index) => {
+            {error2 && <h2 className="errorMessage">Er is iets misgegaan bij het ophalen van de details... Probeer het opnieuw</h2>}
+            <article className="pokemonCard">
+                <h2>{capitalizedName}</h2>
+                <span className="sprite-wrapper">
+                    <img src={pokemonDetails.sprites.other["official-artwork"].front_default} alt={`image of ${pokemonDetails.name}`}/>
+                </span>
+                <div className="inner-container">
+                    <p>Moves: {pokemonDetails.moves.length}</p>
+                    <p>Weight: {pokemonDetails.weight}</p>
+                </div>
+                <div className="inner-container">
+                    <h3>Abilities:</h3>
+                    <ul>
+                    {pokemonDetails.abilities.map((ability, index) => {
                         return (
-                            <li key={index}>{ability}</li>
+                            <li key={index}>{ability.ability.name}</li>
                         );
                     })}
                 </ul>
+                </div>
             </article>
         </>
     );
